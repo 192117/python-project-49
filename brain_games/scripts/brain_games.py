951f0d181780cdd53ba404b12934e798056f9a83
a.kalimullin <noti1996@gmail.com>
2022-10-20 12:11:38 +0300

    Second step in Brain Games on Hexlet!

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,7 @@
+from brain_games.cli import welcome_user
 def main():
     print("poetry run python -m brain_games.scripts.brain_games\nWelcome to the Brain Games!")
-
+    welcome_user()
 
 if __name__ == '__main__':
     main()
