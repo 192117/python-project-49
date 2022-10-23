cf939d5210a792290567580ea9b82c3d8a2e4fa4
arturkalimullin <noti1996@gmail.com>
2022-10-23 17:41:18 +0300

    Seventh step in Brain Games on Hexlet!

---
+++ brain_games/gcd.py
@@ -0,0 +1,26 @@
+import prompt
+import random
+import math
+
+
+def gcd_game():
+    operators = ['+', '-', "*"]
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Find the greatest common divisor of given numbers.')
+    count = 0
+    while count < 3:
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 100)
+        result = math.gcd(number1, number2)
+        print(f'Question: {number1} {number2}')
+        answer_user = int(prompt.string('Your answer: '))
+        if answer_user != result:
+            print(f"'{answer_user}' is wrong answer ;(. Correct answer was '{result}'.")
+            print(f"Let's try again, {name}!")
+            break
+        else:
+            print('Correct!')
+            count += 1
+    else:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.gcd import gcd_game
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    gcd_game()
+
+
+if __name__ == '__main__':
+    main()
